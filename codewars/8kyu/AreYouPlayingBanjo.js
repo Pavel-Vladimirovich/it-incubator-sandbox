442a163488f4395b50e1fb6071c800a503d2adb1
Pavel-Vladimirovich <nesoglasnyj.pavel@gmail.com>
2023-11-25 22:49:27 +0300

    Are You Playing Banjo, Will there be enough space

---
+++ codewars/8kyu/AreYouPlayingBanjo.js
@@ -0,0 +1,17 @@
+// Are You Playing Banjo?
+//
+// ❗ DESCRIPTION:
+// Create a function which answers the question "Are you playing banjo?".
+// If your name starts with the letter "R" or lower case "r", you are playing banjo!
+//
+// The function takes a name as its only argument, and returns one of the following strings:
+//
+// name + " plays banjo"
+// name + " does not play banjo"
+// Names given are always valid strings.
+
+// ✔️ SOLUTIONS:
+
+function areYouPlayingBanjo(name) {
+    return name.at(0).toUpperCase() === 'R' ? name + " plays banjo" : name + " does not play banjo"
+}
