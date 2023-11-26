58f85b66a80aac0ed536796b168c37ecc9388901
Pavel-Vladimirovich <nesoglasnyj.pavel@gmail.com>
2023-11-26 21:04:14 +0300

    Switch it Up

---
+++ codewars/8kyu/SwitchItUp.js
@@ -0,0 +1,18 @@
+// Switch it Up!
+
+// ❗ DESCRIPTION:
+//     DESCRIPTION:
+//     When provided with a number between 0-9, return it in words.
+//
+// Input :: 1
+//
+// Output :: "One".
+//
+// If your language supports it, try using a switch statement.
+
+// ✔️ SOLUTIONS:
+
+function switchItUp(number){
+    const arrStringNumbers = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine']
+    return arrStringNumbers[number]
+}
