86008422661298c06fbf6feca424ca1fceccc4aa
pavel <nesoglasnyj.pavel@gmail.com>
2023-11-28 16:41:27 +0300

    Difference of Volumes of Cuboids

---
+++ codewars/8kyu/makeUpperCase.js
@@ -0,0 +1,11 @@
+// MakeUpperCase
+
+// ❗ DESCRIPTION:
+// Write a function which converts the input string to uppercase.
+
+// ✔️ SOLUTIONS:
+
+function makeUpperCase(str) {
+	return str.toUpperCase()
+}
+
